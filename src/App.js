e629dbc64499144c191cb6135bb1c19cfdafbc4d
Mohamed Ismat <mohamedismat91@gmail.com>
2019-02-12 12:50:50 +0400

    Add initial components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import initialData from './initial-data';
+import Column from './column';
 
 class App extends Component {
+  state = initialData;
+
   render() {
-    return <div>Hello world</div>;
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
   }
 }
 
